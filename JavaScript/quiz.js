e4c7c34440d4cc23d7486d010f74b5b70fd9a2f8
Dylan Davenport <ddavenport161@gmail.com>
2017-07-19 20:41:56 -0400

    Updated quiz.js
    Added multiple if statements for each possible score. Depending on your final score, an alert box will pop up with a message.

--- JavaScript/quiz.js
+++ JavaScript/quiz.js
@@ -1,5 +1,7 @@
 function submitAnswers() {
+    //Set Total Amount Of Questions
     var total = 5;
+    //Set Starting Score Value
     var score = 0;
 
     //Get User Input
@@ -10,6 +12,8 @@ function submitAnswers() {
     var q5 = document.forms["quizForm"]["q5"].value;
 
     //Form Validation
+    /* Iterate through the questions and alert
+       the user if they forget to answer a question */
     for (i = 1; i <= total; i++) {
         if (eval("q" + i) == null || eval("q" + i) == '') {
             alert("You forgot to answer question " + i);
@@ -22,6 +26,8 @@ function submitAnswers() {
     var answers = ["b", "d", "d", "b", "a"];
 
     //Check Answer
+    /* Iterate through the array of correct answers and if the answer is 
+       correct increase the score by one */
     for (i = 1; i <= total; i++) {
         if (eval("q" + i) == answers[i - 1]) {
             score++;
@@ -30,8 +36,24 @@ function submitAnswers() {
     }
     //Display Results
     var results = document.getElementById("results");
-    results.innerHTML = "<h3>You scored <span> "+ score +" </span> out of <span>" + total +"</span></h3>"
+    results.innerHTML = "<h3>You scored <span> " + score + " </span> out of <span>" + total + "</span></h3>"
     alert("You Scored " + score + " Out Of " + total);
+
+    if (score === 5) {
+        alert("PERFECT SCORE!");
+    }
+     if (score === 4) {
+        alert("GREAT WORK!");
+    }
+     if (score === 3) {
+        alert("VERY GOOD!");
+    }
+     if (score === 2) {
+        alert("BETTER LUCK NEXT TIME.");
+    }
+     if (score === 1) {
+        alert("TRY AND STUDY HARDER.");
+    }
 
     return false;
 }
